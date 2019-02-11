ed3240cc42ff2595e06a5f5b3a00d31b0b29b1ad
Carlos Pendola <carlospendola@carloss-macbook-pro.local>
2019-02-11 13:44:42 -0700

    Initial Commit

---
+++ JournalC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JournalC
+//
+//  Created by Carlos Pendola on 2/11/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JournalC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JournalC
+//
+//  Created by Carlos Pendola on 2/11/19.
+//  Copyright © 2019 Carlos Pendola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
